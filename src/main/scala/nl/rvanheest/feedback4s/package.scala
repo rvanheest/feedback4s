c0ae04533dcc118d09d56090c1cdf73eac21d4f0
Richard van Heest <richard.v.heest@gmail.com>
2016-11-25 14:03:05 +0100

    documentation for Rx operators

--- src/main/scala/nl/rvanheest/feedback4s/package.scala
+++ src/main/scala/nl/rvanheest/feedback4s/package.scala
@@ -269,81 +269,232 @@ package object feedback4s {
 	}
 
 	/**
+		* Lifts several Rx operators to `Component`. If the required operator is not implemented,
+		* use the [[`liftRx`]] operator to do so instead.
 		*
 		* @param src the `Component` to apply the operators on
 		* @tparam I the input type of `src`
 		* @tparam O the output type of `src`
 		*/
 	implicit class RxOperators[I, O](val src: Component[I, O]) {
-		def doOnCompleted(consumer: => Unit): Component[I, O] = {
-			liftRx(_.doOnCompleted(consumer))
+
+		/**
+			* Modifies the source `Component` so that it invokes an action when it calls onCompleted.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/doOnCompleted.png" alt="doOnCompleted">
+			*
+			* @param onCompleted the action to invoke when the source `Component` calls onCompleted
+			* @return the source `Component` with the side-effecting behavior applied
+			*/
+		def doOnCompleted(onCompleted: => Unit): Component[I, O] = {
+			liftRx(_.doOnCompleted(onCompleted))
 		}
 
+		/**
+			* Modifies the source `Component` so that it invokes an action for each item and terminal event it emits.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/doOnEach.o.png" alt="doOnEach">
+			*
+			* @param observer the `Observer` to be notified about onNext, onError and onCompleted events
+			*                 on its respective methods before the actual downstream Subscriber gets notified.
+			* @return the source `Component` with the side-effecting behavior applied
+			*/
 		def doOnEach(observer: Observer[O]): Component[I, O] = {
 			liftRx(_.doOnEach(observer))
 		}
 
-		def doOnEach(onNext: O => Unit): Component[I, O] = {
-			liftRx(_.doOnEach(onNext))
+		/**
+			* Modifies the source `Component` so that it invokes an action if it calls onError.
+			* In case the onError action throws, the downstream will receive a composite exception
+			* containing the original exception and the exception thrown by onError.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/doOnError.png" alt="doOnError">
+			*
+			* @param onError the action to invoke if the source `Component` calls onError
+			* @return the source `Component` with the side-effecting behavior applied
+			*/
+		def doOnError(onError: Throwable => Unit): Component[I, O] = {
+			liftRx(_.doOnError(onError))
 		}
 
-		def doOnEach(onNext: O => Unit, onError: Throwable => Unit): Component[I, O] = {
-			liftRx(_.doOnEach(onNext, onError))
+		/**
+			* Modifies the source `Component` so that it invokes an action when it calls onNext.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/doOnNext.png" alt="doOnNext">
+			*
+			* @param onNext the action to invoke when the source `Component` calls onNext
+			* @return the source `Component` with the side-effecting behavior applied
+			*/
+		def doOnNext(onNext: O => Unit): Component[I, O] = {
+			liftRx(_.doOnNext(onNext))
 		}
 
-		def doOnEach(onNext: O => Unit, onError: Throwable => Unit, onCompleted: () => Unit): Component[I, O] = {
-			liftRx(_.doOnEach(onNext, onError, onCompleted))
-		}
-
-		def doOnError(consumer: Throwable => Unit): Component[I, O] = {
-			liftRx(_.doOnError(consumer))
-		}
-
-		def doOnNext(consumer: O => Unit): Component[I, O] = {
-			liftRx(_.doOnNext(consumer))
-		}
-
+		/**
+			* Returns an `Component` that skips the first count items emitted by the source `Component`
+			* and emits the remainder.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/skip.png" alt="drop">
+			*
+			* @param n the number of items to skip
+			* @return an `Component` that is identical to the source `Component` except that it does not
+			*         emit the first count items that the source Observable emits
+			*/
 		def drop(n: Int): Component[I, O] = {
 			liftRx(_.drop(n))
 		}
 
+		/**
+			* Returns an `Component` that skips all items emitted by the source `Component` as long as a
+			* specified condition holds true, but emits all further source items as soon as the
+			* condition becomes false.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/skipWhile.png" alt="dropWhile">
+			*
+			* @param predicate a function to test each item emitted from the source `Component`
+			* @return an `Component` that begins emitting items emitted by the source `Component`
+			*         when the specified predicate becomes false
+			*/
 		def dropWhile(predicate: O => Boolean): Component[I, O] = {
 			liftRx(_.dropWhile(predicate))
 		}
 
+		/**
+			* Filters items emitted by an `Component` by only emitting those that satisfy a specified predicate.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/filter.png" alt="filter">
+			*
+			* @param predicate a function that evaluates each item emitted by the source `Component`,
+			*                  returning true if it passes the filter
+			* @return an `Component` that emits only those items emitted by the source `Component` that
+			*         the filter evaluates as true
+			*/
 		def filter(predicate: O => Boolean): Component[I, O] = {
 			liftRx(_.filter(predicate))
 		}
 
-		def liftRx[Y](f: Observable[O] => Observable[Y]) = {
+		/**
+			* Lifts an operator from Rx into a `Component` and concats this with `src`
+			*
+			* @param f the Rx operator
+			* @tparam Y the output type of the Rx operator
+			* @return the `Component` wrapping the concatenation between `src` and the Rx operator
+			*/
+		def liftRx[Y](f: Observable[O] => Observable[Y]): Component[I, Y] = {
 			src >>> Component(f)
 		}
 
-		def sample(interval: Duration, scheduler: Scheduler = NewThreadScheduler()) = {
+		/**
+			* Returns an `Component` that emits the most recently emitted item (if any) emitted
+			* by the source `Component` within periodic time intervals, where the intervals are
+			* defined on a particular `Scheduler`.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/sample.s.png" alt="sample">
+			*
+			* @param interval the sample rate
+			* @param scheduler the `Scheduler` to use when sampling
+			* @return an `Component` that emits the results of sampling the items emitted by the
+			*         source `Component` at the specified time interval
+			*/
+		def sample(interval: Duration, scheduler: Scheduler = NewThreadScheduler()): Component[I, O] = {
 			liftRx(_.sample(interval, scheduler))
 		}
 
+		/**
+			* Returns an `Component` that emits a specified item before it begins to emit
+			* items emitted by the source `Component`.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/startWith.o.png" alt="startWith">
+			*
+			* @param o the item to emit
+			* @return an `Component` that emits the specified item before it begins to emit
+			*         items emitted by the source `Component`
+			*/
 		def startWith(o: O): Component[I, O] = {
 			liftRx(o +: _)
 		}
 
+		/**
+			* Returns an `Component` that applies a specified accumulator function to the first
+			* item emitted by a source `Component` and a seed value, then feeds the result of
+			* that function along with the second item emitted by the source `Component` into
+			* the same function, and so on until all items have been emitted by the source
+			* `Component`, emitting the result of each of these iterations.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/scanSeed.png" alt="scan">
+			*
+			* @param seed the initial (seed) accumulator item
+			* @param combiner an accumulator function to be invoked on each item emitted by the
+			*                 source `Component`, whose result will be emitted to Observers via
+			*                 onNext and used in the next accumulator call
+			* @tparam Y the initial, accumulator and result type
+			* @return an `Component` that emits initialValue followed by the results of each call
+			*         to the accumulator function
+			*/
 		def scan[Y](seed: Y)(combiner: (Y, O) => Y): Component[I, Y] = {
 			liftRx(_.scan(seed)(combiner))
 		}
 
+		/**
+			* Returns an `Component` that emits only the first count items emitted by the source
+			* `Component`. If the source emits fewer than count items then all of its items are emitted.
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/take.png" alt="take">
+			*
+			* @param n the maximum number of items to emit
+			* @return an `Component` that emits only the first count items emitted by the source
+			*         `Component`, or all of the items from the source `Component` if that `Component` emits fewer than count items
+			*/
 		def take(n: Int): Component[I, O] = {
 			liftRx(_.take(n))
 		}
 
+		/**
+			* Returns an `Component` that emits items emitted by the source `Component`,
+			* checks the specified predicate for each item, and then completes when the condition
+			* is satisfied.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/takeUntil.p.png" alt="">
+			*
+			* @param predicate a function that evaluates an item emitted by the source `Component`
+			*                  and returns a Boolean
+			* @return an `Component` that first emits items emitted by the source `Component`,
+			*         checks the specified condition after each item, and then completes when the
+			*         condition is satisfied
+			*/
 		def takeUntil(predicate: O => Boolean): Component[I, O] = {
 			liftRx(_.takeUntil(predicate))
 		}
 
+		/**
+			* Returns an `Component` that emits items emitted by the source `Component` so
+			* long as each item satisfied a specified condition, and then completes as
+			* soon as this condition is not satisfied.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/takeWhile.png" alt="takeWhile">
+			*
+			* @param predicate a function that evaluates an item emitted by the source `Component` and returns a Boolean
+			* @return an `Component` that emits the items from the source `Component`
+			*         so long as each item satisfies the condition defined by predicate, then completes
+			*/
 		def takeWhile(predicate: O => Boolean): Component[I, O] = {
 			liftRx(_.takeWhile(predicate))
 		}
 
-		def throttleFirst(duration: Duration, scheduler: Scheduler = NewThreadScheduler()) = {
+		/**
+			* Returns an `Component` that emits only the first item emitted by the source `Component`
+			* during sequential time windows of a specified duration, where the windows are managed
+			* by a specified Scheduler.
+			*
+			* This differs from throttleLast(long, java.util.concurrent.TimeUnit) in that this only
+			* tracks passage of time whereas throttleLast(long, java.util.concurrent.TimeUnit) ticks
+			* at scheduled intervals.
+			*
+			* <img src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/throttleFirst.s.png" alt="throttleFirst">
+			*
+			* @param duration time to wait before emitting another item after emitting the last item
+			* @param scheduler the Scheduler to use internally to manage the timers that handle timeout for each event
+			* @return an `Component` that performs the throttle operation
+			*/
+		def throttleFirst(duration: Duration, scheduler: Scheduler = NewThreadScheduler()): Component[I, O] = {
 			liftRx(_.throttleFirst(duration, scheduler))
 		}
 	}
